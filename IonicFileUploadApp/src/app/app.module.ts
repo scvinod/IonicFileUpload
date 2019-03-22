502ae0abb4f3148cffd78f4f541bebbdb937f0e2
scvinod <s.c.vinod@gmail.com>
2019-03-22 21:32:59 +0530

    adding files

--- IonicFileUploadApp/src/app/app.module.ts
+++ IonicFileUploadApp/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Camera } from '../../node_modules/@ionic-native/camera';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
+  providers: [Camera,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- IonicFileUploadApp/src/app/home/home.page.ts
+++ IonicFileUploadApp/src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { NavController, NavParams, ToastController
+, ActionSheetController, AlertController, LoadingController } from '@ionic/angular';
+import { Headers, Http, Response } from '@angular/http';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+  constructor(public http: Http, public toastCtrl: ToastController, 
+  public loadingCtrl: LoadingController, public navCtrl: NavController, 
+  public navParams: NavParams, private camera: Camera, private actionsheetCtrl: ActionSheetController, 
+  private storage: Storage, private alertCtrl: AlertController, public loader: LoadingController){
+
+  }
 }
